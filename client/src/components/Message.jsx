fd516b4b1ca1d5c3dc2440e3f40afe366c14ae18
JKAnderson409 <JKAnderson409@gmail.com>
2018-07-17 12:08:57 -0400

    Shows messages from server

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Message = (props) => {
   return (
     <div className="message">
-      <h3>{props.messageData.user}: </h3>
+      <h2>{props.messageData.roomName}</h2>
+      <h3>{props.messageData.userID}: </h3>
       <p>{props.messageData.text}</p>
     </div>
   )
