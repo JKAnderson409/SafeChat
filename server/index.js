2a244e2ec548030cd23a7cbf1ea981786eff5ef9
Jack Lim <jacklim.tkl@gmail.com>
2018-07-17 14:02:11 -0400

    added postMsg routing

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-var express = require('express')
+var express = require('express');
 var morgan = require('morgan');
 var parser = require('body-parser');
 var cors = require('cors');
@@ -8,26 +8,26 @@ var controller = require('./controllers');
 var app = express();
 let port = process.env.PORT || 4000;
 
-app.set('port',port)
+app.set('port',port);
 app.use(parser.json());
-app.use(parser.urlencoded({ extended: true }));
-app.use(cors())
+app.use(parser.urlencoded({ extended: false }));
+app.use(cors());
 app.use(morgan('dev'));
 
 
-app.use(express.static(__dirname + '/../client/dist'))
+app.use(express.static(__dirname + '/../client/dist'));
 
-app.get('/users')
-app.post('/users')
+app.get('/users');
+app.post('/users');
 
-app.get('/rooms')
-app.post('/rooms')
+app.get('/rooms');
+app.post('/rooms');
 
-app.get('/messages', controller.messages.get)
-app.post('/messages')
+app.get('/messages', controller.messages.get);
+app.post('/messages', controller.messages.post);
 
-app.get('/*',(req,res)=>res.redirect('/'))
+app.get('/*',(req,res)=>res.redirect('/'));
 
-app.listen(port,()=> console.log('Listening on :', port))
+app.listen(port,()=> console.log('Listening on :', port));
 
 module.exports.app = app;
