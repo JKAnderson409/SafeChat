c1f5a9a840159909eec8a7eb287af2be8b87939b
Joseph Mulder <joseph.mulder@stonybrook.edu>
2018-07-17 12:21:35 -0400

    chantes to schema

---
+++ server/ model/index.js
@@ -0,0 +1,28 @@
+let db = require('../../database/database.js');
+
+module.exports = {
+  messages: {
+    get: function(callback) {
+
+    },
+    post: function(data, callback) {
+
+    }
+  },
+  users: {
+    get: function() {
+
+    }, 
+    post: function() {
+
+    }
+  }, 
+  rooms: {
+    get: function() {
+
+    },
+    post: function() {
+
+    }
+  }  
+};
