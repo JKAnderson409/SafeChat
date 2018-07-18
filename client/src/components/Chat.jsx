e3211c836b8e17ca11f961ac9325d8d8cfcd8891
Jack Lim <jacklim.tkl@gmail.com>
2018-07-18 18:49:28 -0400

    calculate room score

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import Sentiment from 'sentiment';
 import { Table } from 'react-bootstrap';
 
 import NewMessage from './NewMessage.jsx';
 import Message from './Message.jsx';
 import Title from './Title.jsx';
 import { resolve } from 'path';
+import textToScore from '../lib/textToScore.js'
 
 export default class Chat extends Component {
   constructor(props){
@@ -49,26 +49,30 @@ export default class Chat extends Component {
       .then(res => { // Checks each message from server against activeRoomId
         // and filters out the ones that don't match, messages for the active
         // room are stored in state.messages
-        var msgs;
-        if(res.data){
-          msgs = res.data.reverse().filter(msg => {
+        // TODO: change sql query to only select current roomID
+        let messages = [];
+        if(res.data.length){
+          messages = res.data.reverse().filter(msg => {
             return (msg.roomId === this.state.activeRoomId);
           });
         }
-        this.setState({
-          messages: msgs
-        }, console.log(msgs));
+        let roomScore = 0;
+        messages.forEach((msg)=>roomScore+=msg.score)
+        /*******************************************/
+        this.setState({messages,roomScore}, console.log(messages));
       })
       .catch(err => {console.error(err)})
   }
 
   handleChange = (event) => {
+    let input = event.target.value;
     this.setState({
-        newMessageText: event.target.value
+        newMessageText: input,
+        messageScore: textToScore(input)
     });
   }
 
-  refreshInput = () => {this.setState({newMessageText: ''});}
+  refreshInput = () => {this.setState({newMessageText: '',messageScore:0});}
 
   postMessage = () => {
     axios.post('/messages', {
